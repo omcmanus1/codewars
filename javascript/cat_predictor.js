ab07149e7c4df96c8488948462bb8f27b00c63b4
Oliver McManus <ollie_mc@Olivers-MacBook-Pro.local>
2022-12-12 11:11:02 +0000

    refactor cat predictor

--- javascript/cat_predictor.js
+++ javascript/cat_predictor.js
@@ -2,10 +2,10 @@ function catPredictor (startDate, endDate) {
     const sD = new Date(startDate)
     const eD = new Date(endDate)
     let baseNum = 0
-    if (sD.getDay() != 0) {baseNum++}
-    if (eD.getDay() != 0) {baseNum++}
+    if (sD.getDay() !== 0) baseNum++
+    if (eD.getDay() !== 0) baseNum++
     let milliSeconds = eD - sD + (baseNum * 86400000)
-    let days = (milliSeconds/(86400000))
+    let days = (milliSeconds / (86400000))
     let calcDays = days - (days / 7)
     return Math.floor(calcDays * 1.15)
 }
