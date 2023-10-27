e29b8601cfbf599c0d725a18e73c7e8d3e9079a7
omcmanus1 <omcmanus1@gmail.com>
2023-10-27 22:42:46 +0100

    cartesian coords

---
+++ go/katas/cartesianNeighbour.go
@@ -0,0 +1,5 @@
+package katas
+
+func CartesianNeighbor(x, y int) [][]int {
+	return [][]int{{x + 1, y}, {x + 1, y - 1}, {x + 1, y + 1}, {x - 1, y}, {x - 1, y - 1}, {x - 1, y + 1}, {x, y - 1}, {x, y + 1}}
+}
--- go/main.go
+++ go/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(katas.CountBits(456))
+	fmt.Println(katas.CartesianNeighbor(1, 2))
 }
