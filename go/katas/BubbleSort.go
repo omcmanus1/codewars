e61a1b561135c5d5d5782722a0708c6f2b1bb762
omcmanus1 <omcmanus1@gmail.com>
2023-10-28 14:25:42 +0100

    fix singlePass bug

--- go/katas/BubbleSort.go
+++ go/katas/BubbleSort.go
@@ -1,7 +1,7 @@
 package katas
 
-func singlePass(arr []int, n int) []int {
-	for i := 0; i < n-1; i++ {
+func singlePass(arr []int, n int, j int) []int {
+	for i := 0; i < n-j-1; i++ {
 		if arr[i] > arr[i+1] {
 			temp := arr[i]
 			arr[i] = arr[i+1]
@@ -15,17 +15,16 @@ func BubblesortOnce(numbers []int) []int {
 	numsCopy := make([]int, len(numbers))
 	copy(numsCopy, numbers)
 	n := len(numsCopy)
-	singlePass(numsCopy, n)
+	singlePass(numsCopy, n, 0)
 	return numsCopy
 }
 
 func Bubblesort(numbers []int) []int {
 	numsCopy := make([]int, len(numbers))
 	copy(numsCopy, numbers)
-	var i int
 	n := len(numsCopy)
-	for i = 0; i < n; i++ {
-		singlePass(numsCopy, i)
+	for j := 0; j < n; j++ {
+		singlePass(numsCopy, n, j)
 	}
 	return numsCopy
 }
