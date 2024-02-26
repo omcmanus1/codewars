74d3bb58fe21328923220a1acd28fe4abf493d27
omcmanus1 <omcmanus1@gmail.com>
2024-02-26 22:50:13 +0000

    partList

---
+++ go/katas/partList.go
@@ -0,0 +1,25 @@
+package katas
+
+import (
+	"strings"
+)
+
+func PartList(inp []string) string {
+	var output string
+
+	if len(inp) <= 1 {
+		return output
+	}
+
+	for i := 1; i < len(inp); i++ {
+		if i == 0 {
+			continue
+		}
+		var tempStr string
+		firstChunk := strings.Join(inp[:i][:], " ")
+		secondChunk := strings.Join(inp[i:][:], " ")
+		tempStr = "(" + firstChunk + ", " + secondChunk + ")"
+		output += tempStr
+	}
+	return output
+}
--- go/main.go
+++ go/main.go
@@ -7,6 +7,6 @@ import (
 )
 
 func main() {
-	testArr := []int{4, 4, 2, -3, 1, 4, 3, 2, 0, -5, 2, -2, -2, -5}
-	fmt.Println(katas.CountInput(testArr))
+	testArr := []string{"az", "toto", "picaro", "zone", "kiwi"}
+	fmt.Println(katas.PartList(testArr))
 }
